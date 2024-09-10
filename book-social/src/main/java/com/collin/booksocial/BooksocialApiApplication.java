d5cf4151d3901bf82e80c687c41c8372fbb08480
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-10 22:38:22 +0200

    Spring setup

---
+++ book-social/src/main/java/com/collin/booksocial/BooksocialApiApplication.java
@@ -0,0 +1,13 @@
+package com.collin.booksocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksocialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksocialApiApplication.class, args);
+	}
+
+}
---
+++ book-social/src/test/java/com/collin/booksocial/BooksocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collin.booksocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksocialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
