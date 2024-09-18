16eb851b90f699b33ba45353cbfa916763922183
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-18 21:09:27 +0200

    Added documentation on AuthenticationRequest

--- booksocial/src/main/java/com/collin/booksocial/auth/AuthenticationRequest.java
+++ booksocial/src/main/java/com/collin/booksocial/auth/AuthenticationRequest.java
@@ -13,11 +13,33 @@ import lombok.Setter;
 @Builder
 public class AuthenticationRequest {
 
+    /**
+     * Represents the email of the user attempting to authenticate.
+     * The email must be in a valid format and cannot be null or empty.
+     *
+     * Constraints:
+     * - Must be a valid email format.
+     * - Cannot be null or empty.
+     *
+     * Validation messages:
+     * - "Email format is invalid" when the email does not conform to standard email structure.
+     * - "Email is mandatory" when the field is null or empty.
+     */
     @Email(message = "Email format is invalid")
     @NotEmpty(message = "Email is mandatory")
     @NotNull(message = "Email is mandatory")
     private String email;
 
+    /**
+     * The password required for authentication.
+     * This field is mandatory and cannot be empty.
+     * It must be at least 8 characters long.
+     *
+     * Validation constraints:
+     * - Must not be null
+     * - Must not be empty
+     * - Must have a minimum length of 8 characters
+     */
     @NotEmpty(message = "Password is mandatory")
     @NotNull(message = "Password is mandatory")
     @Size(min = 8, message = "Password should be at least 8 characters long")
