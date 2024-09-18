c17adddb10c1394c8307c080030bd47f4b8c96e9
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-18 21:10:19 +0200

    Added documentation on AuthenticationResponse

--- booksocial/src/main/java/com/collin/booksocial/auth/AuthenticationResponse.java
+++ booksocial/src/main/java/com/collin/booksocial/auth/AuthenticationResponse.java
@@ -5,6 +5,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Represents a response containing an authentication token.
+ * Typically used to encapsulate the token returned upon successful authentication.
+ *
+ * Uses Lombok annotations to generate getter and setter methods, and a builder pattern.
+ */
 @Getter
 @Setter
 @Builder
