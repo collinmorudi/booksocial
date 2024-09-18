3e3b09376a3914096b0fc6741f3e6cdb8f607a6d
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-18 21:29:14 +0200

    docs: Add javadoc comments to BookResponse and BookService

--- booksocial/src/main/java/com/collin/booksocial/book/BookResponse.java
+++ booksocial/src/main/java/com/collin/booksocial/book/BookResponse.java
@@ -6,6 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * BookResponse represents the response object containing details of a book.
+ * It includes attributes such as id, title, author name, ISBN, synopsis,
+ * owner, cover image, rate, archived status, and shareable status.
+ * This class provides getter and setter methods for accessing and modifying the attributes.
+ * Additionally, it includes annotations for constructing instances with various configurations.
+ */
 @Getter
 @Setter
 @AllArgsConstructor
@@ -14,14 +21,49 @@ import lombok.Setter;
 public class BookResponse {
 
     private Integer id;
+    /**
+     * Represents the title of the book.
+     */
     private String title;
+    /**
+     * The name of the author of the book.
+     */
     private String authorName;
+    /**
+     * The ISBN (International Standard Book Number) of the book.
+     * This is a unique identifier assigned to each edition of a book,
+     * allowing for more efficient marketing and ordering of books.
+     */
     private String isbn;
+    /**
+     * A brief summary or general overview of the book's content.
+     * This description provides potential readers with an insight or
+     * snapshot of what the book is about without revealing critical
+     * plot points or conclusions.
+     */
     private String synopsis;
+    /**
+     * Represents the owner of the book.
+     */
     private String owner;
+    /**
+     * The cover image of the book.
+     * This is stored as a byte array representing the image data.
+     */
     private byte[] cover;
+    /**
+     * Represents the average rating of the book.
+     * This rate is a numerical value that reflects the overall assessment or feedback from readers.
+     */
     private double rate;
+    /**
+     * Indicates whether the book is archived.
+     * This boolean flag is used to mark a book as archived or not.
+     */
     private boolean archived;
+    /**
+     * Indicates whether the book can be shared with others or not.
+     */
     private boolean shareable;
 
 }
--- booksocial/src/main/java/com/collin/booksocial/book/BookService.java
+++ booksocial/src/main/java/com/collin/booksocial/book/BookService.java
@@ -24,17 +24,44 @@ import java.util.Objects;
 import static com.collin.booksocial.book.BookSpecification.withOwnerId;
 
 
+/**
+ * Service class for managing book-related operations.
+ */
 @Service
 @RequiredArgsConstructor
 @Slf4j
 @Transactional
 public class BookService {
 
+    /**
+     * Repository responsible for managing Book entities.
+     * Handles database operations related to books, including CRUD operations.
+     */
     private final BookRepository bookRepository;
+    /**
+     * The BookMapper is responsible for converting between Book entities and their corresponding DTOs.
+     * It is used within the BookService to facilitate data transfer and ensure consistency between
+     * the service layer and other components such as controllers and repositories.
+     */
     private final BookMapper bookMapper;
+    /**
+     * Repository to handle the CRUD operations for BookTransactionHistory entities.
+     * Used within the BookService to manage the history of book transactions such as borrow and return.
+     */
     private final BookTransactionHistoryRepository transactionHistoryRepository;
+    /**
+     * Service responsible for handling file storage operations.
+     * Used within the BookService class for various file-related functionalities such as uploading book cover pictures.
+     */
     private final FileStorageService fileStorageService;
 
+    /**
+     * Saves a new book record to the repository.
+     *
+     * @param request The request object containing book details.
+     * @param connectedUser The authenticated user making the save request.
+     * @return The ID of the saved book.
+     */
     public Integer save(BookRequest request, Authentication connectedUser) {
          User user = ((User) connectedUser.getPrincipal());
         Book book = bookMapper.toBook(request);
@@ -42,12 +69,27 @@ public class BookService {
         return bookRepository.save(book).getId();
     }
 
+    /**
+     * Retrieves a book by its unique identifier.
+     *
+     * @param bookId the unique identifier of the book to be retrieved
+     * @return the BookResponse object representing the found book
+     * @throws EntityNotFoundException if no book is found with the provided ID
+     */
     public BookResponse findById(Integer bookId) {
         return bookRepository.findById(bookId)
                 .map(bookMapper::toBookResponse)
                 .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
     }
 
+    /**
+     * Retrieves a pageable list of all displayable books for the authenticated user.
+     *
+     * @param page the current page number to retrieve
+     * @param size the number of items per page
+     * @param connectedUser the authenticated user
+     * @return a PageResponse containing a list of BookResponse objects and pagination details
+     */
     public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication connectedUser) {
         User user = ((User) connectedUser.getPrincipal());
         Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
@@ -66,6 +108,14 @@ public class BookService {
         );
     }
 
+    /**
+     * Retrieves a paginated list of books owned by the currently authenticated user.
+     *
+     * @param page the page number to retrieve.
+     * @param size the number of records per page.
+     * @param connectedUser the authenticated user object.
+     * @return a PageResponse containing a list of BookResponse objects and pagination details.
+     */
     public PageResponse<BookResponse> findAllBooksByOwner(int page, int size, Authentication connectedUser) {
         User user = ((User) connectedUser.getPrincipal());
         Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
@@ -84,6 +134,14 @@ public class BookService {
         );
     }
 
+    /**
+     * Retrieves a paginated list of all borrowed books for the authenticated user.
+     *
+     * @param page the page number to retrieve
+     * @param size the size of the page to retrieve
+     * @param connectedUser the Authentication object representing the currently authenticated user
+     * @return a PageResponse containing a list of BorrowedBookResponse objects and pagination information
+     */
     public PageResponse<BorrowedBookResponse> findAllBorrowedBooks(int page, int size, Authentication connectedUser) {
         User user = ((User) connectedUser.getPrincipal());
         Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
@@ -102,6 +160,16 @@ public class BookService {
         );
     }
 
+    /**
+     * Updates the shareable status of a book. This method will toggle the current shareable status of the
+     * book identified by the given bookId. The operation is restricted to the owner of the book.
+     *
+     * @param bookId The ID of the book whose shareable status needs to be updated.
+     * @param connectedUser The authenticated user making the request. The user must be the owner of the book.
+     * @return The ID of the book whose shareable status was updated.
+     * @throws EntityNotFoundException If no book is found with the given ID.
+     * @throws OperationNotPermittedException If the authenticated user is not the owner of the book.
+     */
     public Integer updateShareableStatus(Integer bookId, Authentication connectedUser) {
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
@@ -114,6 +182,16 @@ public class BookService {
         return bookId;
     }
 
+    /**
+     * Updates the archived status of a book identified by its ID.
+     * The method toggles the archived status of the book if the authenticated user is the owner.
+     *
+     * @param bookId the ID of the book to update
+     * @param connectedUser the authenticated user attempting to update the archived status
+     * @return the ID of the updated book
+     * @throws EntityNotFoundException if no book is found with the specified ID
+     * @throws OperationNotPermittedException if the authenticated user is not the owner of the book
+     */
     public Integer updateArchivedStatus(Integer bookId, Authentication connectedUser) {
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
@@ -126,6 +204,16 @@ public class BookService {
         return bookId;
     }
 
+    /**
+     * Borrows a book specified by its ID if it is available and conditions are met.
+     *
+     * @param bookId the ID of the book to be borrowed.
+     * @param connectedUser the authenticated user attempting to borrow the book.
+     * @return the ID of the transaction history entry created for this borrowing action.
+     * @throws EntityNotFoundException if no book is found with the specified ID.
+     * @throws OperationNotPermittedException if the book is archived, not shareable,
+     *         already borrowed, or owned by the user attempting to borrow it.
+     */
     public Integer borrowBook(Integer bookId, Authentication connectedUser) {
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
@@ -156,6 +244,16 @@ public class BookService {
 
     }
 
+    /**
+     * Handles the return process of a borrowed book by a user.
+     *
+     * @param bookId          the ID of the book to be returned
+     * @param connectedUser   the authentication object of the user trying to return the book
+     * @return the ID of the book transaction history record updated
+     * @throws EntityNotFoundException      if the book with the specified ID is not found
+     * @throws OperationNotPermittedException if the book is archived or not shareable, if the book is owned by the user,
+     *                                        or if the user did not borrow the book
+     */
     public Integer returnBorrowedBook(Integer bookId, Authentication connectedUser) {
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
@@ -173,6 +271,15 @@ public class BookService {
         bookTransactionHistory.setReturned(true);
         return transactionHistoryRepository.save(bookTransactionHistory).getId();
     }
+/**
+ * Approves the return of a borrowed book given its ID.
+ *
+ * @param bookId the ID of the book to approve return for
+ * @param connectedUser the authenticated user initiating the request
+ * @return the ID of the transaction history record after saving the approval status
+ * @throws EntityNotFoundException if no book with the specified ID is found
+ * @throws OperationNotPermittedException if the book is archived, not shareable, or the user does not own the book
+ */
 //
     public Integer approveReturnBorrowedBook(Integer bookId, Authentication connectedUser) {
         Book book = bookRepository.findById(bookId)
@@ -191,6 +298,13 @@ public class BookService {
         bookTransactionHistory.setReturnApproved(true);
         return transactionHistoryRepository.save(bookTransactionHistory).getId();
     }
+/**
+ * Uploads a new cover picture for the specified book.
+ *
+ * @param file the MultipartFile containing the book cover image
+ * @param connectedUser the authenticated user making the request
+ * @param bookId the ID of the book for which the cover image is being uploaded
+ */
 //
     public void uploadBookCoverPicture(MultipartFile file, Authentication connectedUser, Integer bookId) {
         Book book = bookRepository.findById(bookId)
@@ -201,24 +315,14 @@ public class BookService {
         bookRepository.save(book);
     }
 
-//    public PageResponse<BorrowedBookResponse> findAllBorrowedBooks(int page, int size, Authentication connectedUser) {
-//        // User user = ((User) connectedUser.getPrincipal());
-//        Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
-//        Page<BookTransactionHistory> allBorrowedBooks = transactionHistoryRepository.findAllBorrowedBooks(pageable, connectedUser.getName());
-//        List<BorrowedBookResponse> booksResponse = allBorrowedBooks.stream()
-//                .map(bookMapper::toBorrowedBookResponse)
-//                .toList();
-//        return new PageResponse<>(
-//                booksResponse,
-//                allBorrowedBooks.getNumber(),
-//                allBorrowedBooks.getSize(),
-//                allBorrowedBooks.getTotalElements(),
-//                allBorrowedBooks.getTotalPages(),
-//                allBorrowedBooks.isFirst(),
-//                allBorrowedBooks.isLast()
-//        );
-//    }
-
+    /**
+     * Retrieves a paginated list of all books that have been returned by the current user.
+     *
+     * @param page The page number to retrieve, starting from zero.
+     * @param size The number of records per page.
+     * @param connectedUser The authentication object representing the currently connected user.
+     * @return A paginated response containing the list of returned books and pagination information.
+     */
     public PageResponse<BorrowedBookResponse> findAllReturnedBooks(int page, int size, Authentication connectedUser) {
          User user = ((User) connectedUser.getPrincipal());
         Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
