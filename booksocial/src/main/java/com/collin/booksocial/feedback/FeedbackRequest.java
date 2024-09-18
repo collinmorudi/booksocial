d90fc94741fdea052cc2b49212ffe2fb4feb68d5
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-18 21:44:06 +0200

    docs: Add javadoc comments to FeedBackResponse and FeedBackResponse

--- booksocial/src/main/java/com/collin/booksocial/feedback/FeedbackRequest.java
+++ booksocial/src/main/java/com/collin/booksocial/feedback/FeedbackRequest.java
@@ -7,6 +7,24 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+/**
+ * FeedbackRequest represents a request to provide feedback consisting of a rating (note),
+ * comment, and the associated book identifier (bookId).
+ *
+ * @param note    The rating provided as feedback. It must be a positive number between 0 and 5.
+ *                Validation messages:
+ *                - "200" for any positive number.
+ *                - "201" if the value is below the minimum value.
+ *                - "202" if the value exceeds the maximum value.
+ *
+ * @param comment The comment provided as feedback. It cannot be null, empty, or blank.
+ *                Validation message:
+ *                - "203" for any violation of null, empty, or blank constraints.
+ *
+ * @param bookId  The identifier of the book the feedback is related to. It cannot be null.
+ *                Validation message:
+ *                - "204" if the bookId is null.
+ */
 public record FeedbackRequest(
         @Positive(message = "200")
         @Min(value = 0, message = "201")
--- booksocial/src/main/java/com/collin/booksocial/feedback/FeedbackResponse.java
+++ booksocial/src/main/java/com/collin/booksocial/feedback/FeedbackResponse.java
@@ -6,6 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * FeedbackResponse class represents a user's feedback, including a numeric rating, a written comment,
+ * and a flag indicating if the feedback was provided by the user themselves.
+ *
+ * It uses Lombok annotations for boilerplate code reduction such as getters, setters, constructors, and the builder pattern.
+ *
+ * - note: A numeric rating (as a Double) of the feedback.
+ * - comment: A textual comment associated with the feedback message.
+ * - ownFeedback: A boolean flag indicating if the feedback was provided by the user.
+ */
 @Getter
 @Setter
 @AllArgsConstructor
