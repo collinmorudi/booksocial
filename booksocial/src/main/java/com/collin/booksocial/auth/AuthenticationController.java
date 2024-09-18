b861209e1a77d1b22472a311ecde1b4fb03a444d
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-18 21:08:28 +0200

    Added documentation on AuthenticationController

--- booksocial/src/main/java/com/collin/booksocial/auth/AuthenticationController.java
+++ booksocial/src/main/java/com/collin/booksocial/auth/AuthenticationController.java
@@ -8,14 +8,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * REST controller for handling authentication-related operations.
+ */
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
 @Tag(name = "Authentication")
 public class AuthenticationController {
 
+    /**
+     * Service responsible for handling authentication operations such as user registration,
+     * authentication, and account activation.
+     */
     private final  AuthenticationService service;
 
+    /**
+     * Handles the registration process for new users.
+     *
+     * @param request the registration request object containing the user's details
+     * @return a ResponseEntity indicating the registration status
+     * @throws MessagingException if there is an issue with sending a confirmation email
+     */
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseEntity<?> register(
@@ -25,6 +39,12 @@ public class AuthenticationController {
         return ResponseEntity.accepted().build();
     }
 
+    /**
+     * Authenticates a user based on the provided authentication request.
+     *
+     * @param request the authentication request object containing the user's credentials
+     * @return a ResponseEntity containing the authentication response with authentication details
+     */
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate (
             @RequestBody @Valid AuthenticationRequest request
@@ -32,6 +52,12 @@ public class AuthenticationController {
         return ResponseEntity.ok(service.authenticate(request));
     }
 
+    /**
+     * Endpoint for activating a user's account using an activation token.
+     *
+     * @param token the activation token provided in the request
+     * @throws Throwable if an error occurs during the activation process
+     */
     @GetMapping("/activate-account")
     public void confirm(
             @RequestParam String token
