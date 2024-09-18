e6212476c4743df032a98e01c6032cc09524171a
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-18 21:54:30 +0200

    docs: Add javadoc comments to TokenRepository and UserRepository

--- booksocial/src/main/java/com/collin/booksocial/repository/TokenRepository.java
+++ booksocial/src/main/java/com/collin/booksocial/repository/TokenRepository.java
@@ -5,6 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * Repository interface for managing Token entities.
+ *
+ * This interface extends JpaRepository providing several methods
+ * for working with Token persistence, including methods for saving,
+ * deleting, and finding Token entities.
+ *
+ * The custom method findByToken(String token) is used to find a
+ * Token entity based on the token string.
+ */
 public interface TokenRepository extends JpaRepository<Token, Integer> {
 
     Optional findByToken(String token);
--- booksocial/src/main/java/com/collin/booksocial/repository/UserRepository.java
+++ booksocial/src/main/java/com/collin/booksocial/repository/UserRepository.java
@@ -5,6 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * UserRepository is an interface for performing CRUD operations on User entities.
+ * It extends the JpaRepository interface to inherit standard database operations.
+ *
+ * Methods:
+ *
+ *
+ * findByEmail(String email):
+ *     Retrieves an Optional containing a User entity based on the provided email.
+ *     If no User is found, the Optional is empty.
+ */
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional findByEmail(String email);
