90d2f102942baa964ed52b6a38bd36be9f85883a
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-18 14:22:52 +0200

    Add OpenApi documentation

---
+++ booksocial/src/main/java/com/collin/booksocial/config/OpenApiConfig.java
@@ -0,0 +1,64 @@
+package com.collin.booksocial.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Collin",
+                        email = "collinmorudi@gmail.com",
+                        url = "https://github.com/collinmorudi"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification - Collin Morudi",
+                version = "1.0",
+                license = @License(
+                        name = "To be added",
+                        url = "https://tobeadded"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088/api/v1"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://tobeadded"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description is going to be added",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        flows = @OAuthFlows(
+                clientCredentials =
+                @OAuthFlow(
+                        authorizationUrl = "http://localhost:9090/realms/book-social-network/protocol/openid-connect/auth"
+                )
+        ),
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
