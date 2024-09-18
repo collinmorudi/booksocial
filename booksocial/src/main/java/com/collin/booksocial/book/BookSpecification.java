6af2d019fafce485f8cde44e2140e8af78c9b64c
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-18 21:31:04 +0200

    docs: Add javadoc comments to BookSpecification and BorrowedBookResponse

--- booksocial/src/main/java/com/collin/booksocial/book/BookSpecification.java
+++ booksocial/src/main/java/com/collin/booksocial/book/BookSpecification.java
@@ -2,8 +2,21 @@ package com.collin.booksocial.book;
 
 import org.springframework.data.jpa.domain.Specification;
 
+/**
+ * The BookSpecification class provides methods to create specifications for querying Book entities.
+ * This class contains static methods that return specific predicate conditions to be used in JPA criteria queries.
+ *
+ * The current implementation includes a method to generate a specification to fetch books by their owner's ID.
+ */
 public class BookSpecification {
 
+    /**
+     * Creates a specification used to query books based on the owner's ID.
+     * This method generates a predicate that compares the owner's ID of a book against the provided value.
+     *
+     * @param ownerId the ID of the owner to filter books by
+     * @return a specification for filtering books by the owner's ID
+     */
     public static Specification<Book> withOwnerId(Integer ownerId) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("owner").get("id"), ownerId);
     }
--- booksocial/src/main/java/com/collin/booksocial/book/BorrowedBookResponse.java
+++ booksocial/src/main/java/com/collin/booksocial/book/BorrowedBookResponse.java
@@ -6,6 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Represents the response details for a borrowed book.
+ *
+ * This class contains information about a book that has been borrowed,
+ * including its identification details, title, author's name, ISBN,
+ * borrowing rate, and the status of its return.
+ */
 @Getter
 @Setter
 @AllArgsConstructor
@@ -13,11 +20,41 @@ import lombok.Setter;
 @Builder
 public class BorrowedBookResponse {
 
+    /**
+     * The unique identifier for the borrowed book response.
+     */
     private Integer id;
+    /**
+     * The title of the borrowed book.
+     */
     private String title;
+    /**
+     * The name of the author of the borrowed book.
+     */
     private String authorName;
+    /**
+     * The International Standard Book Number (ISBN) of the borrowed book.
+     *
+     * This is a unique identifier for books, allowing for efficient
+     * management and tracking of borrowed books in the system.
+     */
     private String isbn;
+    /**
+     * The borrowing rate of the book.
+     *
+     * This variable represents the rate at which the book has been borrowed,
+     * potentially indicating the frequency or cost of borrowing.
+     */
     private double rate;
+    /**
+     * Indicates whether the borrowed book has been returned.
+     *
+     * This variable holds the status of the borrowed book's return.
+     * If true, the book has been returned; if false, it has not been returned.
+     */
     private boolean returned;
+    /**
+     * Indicates whether the return of the borrowed book has been approved.
+     */
     private boolean returnApproved;
 }
