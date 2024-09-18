c9f0e6f1cbae6d87402cfaff5fc37721252892ec
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-18 21:53:00 +0200

    docs: Add javadoc comments to BookTransactionHistory

--- booksocial/src/main/java/com/collin/booksocial/history/BookTransactionHistory.java
+++ booksocial/src/main/java/com/collin/booksocial/history/BookTransactionHistory.java
@@ -12,6 +12,18 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+/**
+ * Represents the history of transactions for a book, recording details such as the user involved,
+ * the book in question, and the status of the book's return.
+ *
+ * Utilizes Lombok annotations for boilerplate code reduction and JPA annotations for ORM mapping.
+ *
+ * Fields:
+ * - user: The user involved in the book transaction.
+ * - book: The book involved in the transaction.
+ * - returned: A boolean indicating if the book has been returned.
+ * - returnApproved: A boolean indicating if the return has been approved.
+ */
 @Getter
 @Setter
 @SuperBuilder
@@ -20,14 +32,29 @@ import lombok.experimental.SuperBuilder;
 @Entity
 public class BookTransactionHistory extends BaseEntity {
 
+    /**
+     * The user involved in the book transaction.
+     */
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+    /**
+     * Represents the book involved in the transaction.
+     * This field is mapped to the 'book_id' column in the corresponding database table.
+     *
+     * Managed as a many-to-one relationship, allowing multiple transaction records to reference the same book.
+     */
     @ManyToOne
     @JoinColumn(name = "book_id")
     private Book book;
 
 
+    /**
+     * Indicates whether the book involved in the transaction has been returned.
+     */
     private boolean returned;
+    /**
+     * Indicates whether the return of the book has been approved.
+     */
     private boolean returnApproved;
 }
--- booksocial/src/main/java/com/collin/booksocial/repository/RoleRepository.java
+++ booksocial/src/main/java/com/collin/booksocial/repository/RoleRepository.java
@@ -5,6 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * RoleRepository is the interface for Role entity data access operations.
+ *
+ * This repository extends JpaRepository which provides JPA functionalities
+ * for the Role entity, such as CRUD operations.
+ *
+ * Additionally, RoleRepository provides method to find role based on its name.
+ */
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     Optional<Role> findByName(String role);
