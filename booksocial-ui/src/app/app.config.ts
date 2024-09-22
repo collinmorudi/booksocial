25cbf7728af6b653fc632f49bc68581200af63a2
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-09-22 15:26:23 +0200

    Add login implementation

--- booksocial-ui/src/app/app.config.ts
+++ booksocial-ui/src/app/app.config.ts
@@ -1,9 +1,27 @@
+// import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+//
+// import { routes } from './app.routes';
+// import { provideClientHydration } from '@angular/platform-browser';
+//
+// export const appConfig: ApplicationConfig = {
+//   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+// };
+
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {provideHttpClient, withFetch} from '@angular/common/http'; // Import provideHttpClient
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()) // Add provideHttpClient to providers
+  ]
 };
+
